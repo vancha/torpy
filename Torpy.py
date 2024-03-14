b183cfeea7298ecfc5f7352f3e56b67bd2d27be9
Vancha <tjipkevanderheide90@gmail.com>
2024-03-14 15:29:43 +0100

    Add files via upload

--- Torpy.py
+++ Torpy.py
@@ -4,23 +4,27 @@ import Utils
 import Peer
 import Message
 import Client  
-from Tracker import Tracker
-        
+from Tracker import Tracker, TrackerResponseType
+import bencodepy
+ 
 
 
 #Torrent client
 class Torpy:
     #parses the metainfo file, returns the bdecoded results with all values as a dictionary
-    def parse_metainfo_file(metainfo_file_location):
-        pass
-    
+    def parse_metainfo_file( metainfo_file_location):
+        file_handle = open(metainfo_file_location, "rb")
+        try:
+            return bencodepy.decode(file_handle.read())
+        except:
+            exit('Could not properly parse the torrent file.') 
+
     def __init__(self, metainfo_file_location, peer_id):
         self.parsed_metainfo_file = Torpy.parse_metainfo_file(metainfo_file_location)
-        self.tracker_response = Tracker.send_tracker_request()
-
+        self.tracker_response = Tracker.send_tracker_request(self.parsed_metainfo_file)
+        print(f'Tracker response: {self.tracker_response}')
         #if we got an actual peer list(with a sucessful tracker response), start the peer wire protocol
-        if self.tracker_response.type == TrackerResponseType.SUCCESS:
-            self.start_peer_wire_protocol()
+        self.start_peer_wire_protocol()
 
     #gets peers from self.tracker_response, opens a thread for each of them with a call to peer.connect
     def start_peer_wire_protocol(self):
--- Tracker.py
+++ Tracker.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from Constants import PEER_ID
+import requests
+import hashlib
+import bencodepy
+import urllib.parse
 
 class TrackerResponseType(Enum):
     FAIL = 1,
@@ -10,8 +15,30 @@ class TrackerResponse:
         pass
 
 class Tracker:
-    #a dict that must always have a field called type
-    def send_tracker_request():
-        pass
 
+    #a dict that must always have a field called type
+    def send_tracker_request(parsed_metainfo_file):
+        announce_url    = parsed_metainfo_file[b'announce'].decode('utf-8')
+
+        _info_value     = parsed_metainfo_file[b'info']
+        _encoded_value  = bencodepy.encode(_info_value)
+        info_hash       = hashlib.sha1(_encoded_value).hexdigest()
+        info_hash       = urllib.parse.quote(bytearray.fromhex(info_hash))
+        port            = 6881
+        try:
+            port = parsed_metainfo_file[b'port']
+        except:
+            pass
+
+        #@todo: temp variables to simplify getting a connection up
+        uploaded        = 0
+        downloaded      = 0
+        left            = parsed_metainfo_file[b'info'][b'length']
+
+        request_url = f"{announce_url}?info_hash={info_hash}&peer_id={PEER_ID}&port={port}&uploaded={uploaded}&downloaded={downloaded}&left={left}&event=started"
+        
+        try:
+            return bencodepy.decode(requests.get(request_url).content)
+        except Exception as e:
+            exit(f'Failed getting a proper response from the tracker: {e}')
 
