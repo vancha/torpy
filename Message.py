caf4cf1a74465771b91c306eeee5888bda7f6e38
Vancha <tjipkevanderheide90@gmail.com>
2024-03-18 16:41:15 +0100

    Add files via upload

--- Message.py
+++ Message.py
@@ -19,10 +19,20 @@ class MessageType(Enum):
 
 class Message(Enum):
     #builds a message from a socket connection. Blocks until a message has been received
-    def __init__(self, socket):
+    def from_socket(self, socket):
         #get length prefix
-        length_prefix = Utils.bytes_to_int(socket.recv(4))
-        #
+        response = socket.recv(4)#Utils.bytes_to_int(socket.recv(4))
+        length_prefix = Utils.bytes_to_int(response[0:4])
+        if not length_prefix:
+            self.type = MessageType.KEEPALIVE
+            return self
+        self.payload    = socket.recv(length_prefix)
+        self.message_id = payload[0]
+        self.length_prefix = length_prefix
+        
+        return PeerMessage(length_prefix = length_prefix, message_id = message_id, payload=payload[1:])
+        
+        
     
     #returns the message_type
     def get_type(self):
--- Utils.py
+++ Utils.py
@@ -1,5 +1,6 @@
-def bytes_to_int():
-    pass
+#expects four bytes big endian value
+def bytes_to_int(value):
+    return int.from_bytes(value, 'big')
 
 #a dict that must always have a field called type
 def send_tracker_request():
