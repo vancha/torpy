56c1f6811359e5a72cb7ac8189655e4d0a9820f0
Vancha <tjipkevanderheide90@gmail.com>
2024-04-05 16:37:00 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ def send_tracker_request(parsed_metainfo_file):
         except Exception as e:
             exit(f'Failed getting a proper response from the tracker: {e}')
 
-def send_handshake(self, socket, info_hash):
+def send_handshake(socket, info_hash):
         request         = PSTRLEN + PSTR + bytes([0,0,0,0,0,0,0,0]) + info_hash + Constants.PEER_ID.encode('utf-8')
         socket.sendall(request)
 
